b952cd2206609c5f6dde03838c3a6bd016b06e86
Alex Liu <lxiangcn@gmail.com>
2016-08-21 00:49:02 +0800

    页面优化

--- app/utils/StoreFormat.js
+++ app/utils/StoreFormat.js
@@ -1,26 +1,25 @@
 'use strict';
 
 //进行格式化商家的商品数据-满足section进行使用
-export function formatStore(storeData){
-        let goodTagMap = []
-        storeData.food_spu_tags.forEach(spu_tag => {
-          spu_tag.spus.forEach(spu => {
-              if (!goodTagMap[spu_tag.name]) {
-                  goodTagMap[spu_tag.name] = []
-              }
-              goodTagMap[spu_tag.name].push(spu)      
-          })
-       })
-       return goodTagMap;
+export function formatStore(storeData) {
+    let goodTagMap = []
+    storeData.food_spu_tags.forEach(spu_tag => {
+        spu_tag.spus.forEach(spu => {
+            if (!goodTagMap[spu_tag.name]) {
+                goodTagMap[spu_tag.name] = []
+            }
+            goodTagMap[spu_tag.name].push(spu)
+        })
+    })
+    return goodTagMap;
 }
 
-export function calculateGood(storeData){
-        let value = 0;
-        storeData.food_spu_tags.forEach(spu_tag => {
-          spu_tag.spus.forEach(spu => {
-              value ++ ;    
-          })
-       })
-       return value;
+export function calculateGood(storeData) {
+    let value = 0;
+    storeData.food_spu_tags.forEach(spu_tag => {
+        spu_tag.spus.forEach(spu => {
+            value++;
+        })
+    })
+    return value;
 }
-
